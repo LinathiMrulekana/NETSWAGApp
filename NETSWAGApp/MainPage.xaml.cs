f42e3f1afab3eba9e998f8a79b1a1ff115b2b517
LinathiMrulekana <102727009+LinathiMrulekana@users.noreply.github.com>
2022-05-26 15:41:05 +0200

    added commit

--- NETSWAGApp/MainPage.xaml.cs
+++ NETSWAGApp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NETSWAGApp.data;
+using NETSWAGApp.models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,11 +12,30 @@ namespace NETSWAGApp
 {
     public partial class MainPage : ContentPage
     {
+        private object orderingItem;
+
         public MainPage()
         {
             InitializeComponent();
         }
 
-    
+        async void Save_ClickedAsync(object sender, EventArgs e)
+        {
+            var OrderingItem = (OrderingItem)BindingContext;
+            OrderingItemDatabase database = await OrderingItemDatabase.Instance;
+            await database.SaveItemAsync(orderingItem);
+            await Navigation.PopAsync();
+
+        }
+
+         async void Cancel_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private void startDatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+        }
     }
 }
